7694fc4f30442879b0a9149a79d022fdab5f432d
Mikhail_Proshin <mikhail.proshin@ah.nl>
2024-03-29 22:14:26 +0400

    4.1

---
+++ productsService/scripts/populate-products.js
@@ -0,0 +1,24 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { productsList } from "../productsMock.js";
+
+
+const client = new DynamoDBClient({
+    region: "eu-west-1",
+});
+const docClient = DynamoDBDocumentClient.from(client);
+
+async function populateProducts() {
+    try {
+      for (const product of productsList) {
+        const data = await docClient.send(new PutCommand({
+            TableName: "products",
+            Item: product
+        }));
+        console.log('result : ' + JSON.stringify(data));}
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+populateProducts();
