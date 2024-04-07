c880313c12daaef84376a681ff623b93baf26aa9
mihan-pro <mihan-pro@mail.ru>
2024-04-07 10:12:37 +0400

    5.2

--- importService/importFileParser.js
+++ importService/importFileParser.js
@@ -1,4 +1,30 @@
+import csvParser from "csv-parser";
+import { S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({ region: "eu-west-1" });
+
 export async function importFileParser(event) {
+  const bucketName = event.Records[0].s3.bucket.name;
+  const objectKey = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " "),
+  );
+
+  const params = {
+    Bucket: bucketName,
+    Key: objectKey,
+  };
+
+  try {
+    const s3Stream = s3.getObject(params).createReadStream();
+
+    s3Stream
+      .pipe(csvParser())
+      .on("data", (data) => console.log(data))
+      .on("end", () => console.log("CSV processing completed"));
+  } catch (error) {
+    console.log(error);
+    return;
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(
