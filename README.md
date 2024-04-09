1ca39c38ea92a423df3acdad1ac2081dddc4baae
mihan-pro <mihan-pro@mail.ru>
2024-04-09 11:57:49 +0400

    5.3

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 Frontend-link: d1nlhvt4r28ivi.cloudfront.net
 
+**Product Service**
 GET - https://giryujxd03.execute-api.eu-west-1.amazonaws.com/dev/products
 GET - https://giryujxd03.execute-api.eu-west-1.amazonaws.com/dev/products/{id}
 POST - https://giryujxd03.execute-api.eu-west-1.amazonaws.com/dev/products
+
+**Import Service**
+GET - https://szxt7ayn71.execute-api.eu-west-1.amazonaws.com/dev/import
+
 
 Example Id: d345f48d-a3b8-4a24-a6e0-716dea309502
 
--- importService/importFileParser.js
+++ importService/importFileParser.js
@@ -1,39 +1,57 @@
-import csvParser from "csv-parser";
-import { S3Client } from "@aws-sdk/client-s3";
+import csv from "csv-parser";
+import {
+  S3Client,
+  GetObjectCommand,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import { finished } from "stream/promises";
 
-const s3 = new S3Client({ region: "eu-west-1" });
+const client = new S3Client({ region: "eu-west-1" });
 
 export async function importFileParser(event) {
-  const bucketName = event.Records[0].s3.bucket.name;
-  const objectKey = decodeURIComponent(
-    event.Records[0].s3.object.key.replace(/\+/g, " "),
-  );
-
-  const params = {
-    Bucket: bucketName,
-    Key: objectKey,
-  };
-
   try {
-    const s3Stream = s3.getObject(params).createReadStream();
+    const bucketName = event.Records[0].s3.bucket.name;
+    const objectKey = decodeURIComponent(
+      event.Records[0].s3.object.key.replace(/\+/g, " "),
+    );
+    const data = [];
 
-    s3Stream
-      .pipe(csvParser())
-      .on("data", (data) => console.log(data))
-      .on("end", () => console.log("CSV processing completed"));
+    const object = await client.send(
+      new GetObjectCommand({
+        Bucket: bucketName,
+        Key: objectKey,
+      }),
+    );
+
+    await finished(
+      object.Body.pipe(csv({ separator: ";" }))
+        .on("data", (row) => {
+          data.push(row);
+        })
+        .on("error", (error) => {
+          throw error;
+        })
+        .on("end", () => console.log("CSV processing completed", data)),
+    );
+
+    await client.send(
+      new CopyObjectCommand({
+        Bucket: bucketName,
+        CopySource: `${bucketName}/${objectKey}`,
+        Key: objectKey.replace("uploaded", "parsed"),
+      }),
+    );
+
+    await client.send(
+      new DeleteObjectCommand({
+        Bucket: bucketName,
+        Key: objectKey,
+      }),
+    );
   } catch (error) {
-    console.log(error);
-    return;
+    console.log("Error: ", error);
+  } finally {
+    client.destroy();
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "importFileParser handler executed successfully!",
-        input: event,
-      },
-      null,
-      2,
-    ),
-  };
 }
